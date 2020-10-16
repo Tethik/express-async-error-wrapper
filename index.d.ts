268c722c98e3c36fdb8ff4a6a8bc66ad77a3737a
Joakim Uddholm <tethik@gmail.com>
2020-10-16 15:49:04 +0200

    allow function, incase the handler is in pure javascript and not defined as RequestHandler

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
 import { RequestHandler } from "express";
 
-declare function errorWrap(fn: RequestHandler): RequestHandler
+declare function errorWrap(fn: RequestHandler|Function): RequestHandler
 export default errorWrap;
